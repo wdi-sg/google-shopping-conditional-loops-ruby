ca8fd9284ed11ed80971f61b7636038b8bfae424
ElaineChoo <elainechoo85@gmail.com>
2018-04-10 19:39:50 +0800

    homework completed

--- ruby/google-shopping.rb
+++ ruby/google-shopping.rb
@@ -61,30 +61,3 @@ items.each {|item| q6_result.push ({
 })}
 
 pp q6_result
-
-
-
-
-
-# def count_all_shopping_products
-#    puts $data.select{|item| item[:kind] == 'shopping#product'}.length
-# end
-
-# byebug
-
-# def print_titles_of_backorders
-#    pp $data.select{|item| item[:product][:inventories][0][:availability] == 'backorder'}.map{|obj| obj[:product][:title]}
-# end
-
-# def print_titles_of_items_with_more_than_one_image
-#    pp $data.select{|item| item[:product][:images].length > 1}.map{|obj| obj[:product][:title]}
-# end
-
-# def find_products_by_brand(brand = 'Canon')
-#    pp $data.select{|item| item[:product][:brand].downcase == brand.downcase}
-# end
-
-# def print_brand_price_image_of_all_products
-#    pp $data.map{|obj| "brand: #{obj[:product][:brand]}, price: #{obj[:product][:inventories][0][:price]}, image: #{obj[:product][:images][0][:link]}"}
-# end
-
