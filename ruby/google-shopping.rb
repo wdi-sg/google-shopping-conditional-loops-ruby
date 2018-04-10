0a1b5735491fd734331a1836eb33d5bd2650a69c
RequiemDK <kok.chee.kean@outlook.com>
2018-04-10 14:13:18 +0800

    solution

--- ruby/google-shopping.rb
+++ ruby/google-shopping.rb
@@ -2,12 +2,31 @@ require 'byebug'
 require 'json'
 require 'pp'
 
-file = File.read('products.json')
+file = File.read('../products.json')
 data = JSON.parse(file, {:symbolize_names => true})
 
-pp data
+$data = data[:items]
 
 # use byebug to set a breakpoint
 #byebug
 
 # Write your solutions below
+def count_all_shopping_products
+    puts $data.select{|item| item[:kind] == 'shopping#product'}.length
+end
+
+def print_titles_of_backorders
+    pp $data.select{|item| item[:product][:inventories][0][:availability] == 'backorder'}.map{|obj| obj[:product][:title]}
+end
+
+def print_titles_of_items_with_more_than_one_image
+    pp $data.select{|item| item[:product][:images].length > 1}.map{|obj| obj[:product][:title]}
+end
+
+def find_products_by_brand(brand = 'Canon')
+    pp $data.select{|item| item[:product][:brand].downcase == brand.downcase}
+end
+
+def print_brand_price_image_of_all_products
+    pp $data.map{|obj| "brand: #{obj[:product][:brand]}, price: #{obj[:product][:inventories][0][:price]}, image: #{obj[:product][:images][0][:link]}"}
+end
